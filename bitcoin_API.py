df95964fa4962952af2974d4cca39c8d99320a57
mmcel <mm69@yahoo.com>
2025-09-29 13:30:27 -0500

    Created catfact.py and bitcoin_API.py for week 5 assignments

---
+++ bitcoin_API.py
@@ -0,0 +1,16 @@
+import requests
+
+data = requests.get('https://claraj.github.io/mock-bitcoin/currentprice.json').json()
+print(data)
+
+usd_rate = float(data['bpi']['USD']['rate'].replace(',', ''))
+
+print()
+
+print(f'The current Bitcoin price in USD is: {usd_rate}')
+
+dollars = float(input('Enter an amount in USD: '))
+
+bitcoins = round(dollars / usd_rate, 6)
+
+print(f'{dollars} dollars in Bitcoin is: {bitcoins}')
