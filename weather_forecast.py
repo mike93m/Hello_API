a549293f1b948ea8f1383c4f84773fcbbc5ee8e4
mmcel <mm69@yahoo.com>
2025-10-01 22:38:36 -0500

    Working on cleaning up the formatting for weather_forecast.py

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,11 +1,12 @@
 import os
 import requests
 from pprint import pprint
+from datetime import datetime
 
 # Minneapolis Latitude and Longitude
 lat = 44.97
 lon = -93.26
-units = 'metric'  # change to 'imperial' for quantities in Fahrenheit, miles per hour etc. 
+units = 'imperial'  # change to 'imperial' for quantities in Fahrenheit, miles per hour etc. 
 
 api_key = os.environ['WEATHER_KEY']  # Set this environment variable on your computer 
 
@@ -24,8 +25,29 @@ wind_gusts = weather_forecast['list'][0]['wind']['gust']
 # print(f'Current wind: {wind_key}')
 # print(f'Current wind gusts: {wind_gusts}')
 
-for interval in weather_forecast['list']:
-    dt_txt = interval['dt_txt']
+print("Here is the 5 day weather forecast for Minneapolis, MN:")
+print("The weather forecast is provided in 3 hour intervals.\n")
+
+# Print a header for the forecast table
+print(f"{'Time':<10} {'Day':<10} {'Date':<15} {'Temp (°F)':<12} {'Description':<25} {'Wind (m/s)':<12} {'Gusts (m/s)':<12}")
+print("-" * 101)
+
+# Loop through the list of weather data and print the forecast
+for interval in weather_forecast['list']:  # Each interval is a 3 hour forecast
+
+    # Extract the time and date and convert from Unix timestamp to readable format
+    time_stamp = interval['dt'] 
+    time = datetime.fromtimestamp(time_stamp) 
+    formatted_time = time.strftime("%I:%M %p") # Format time as HH:MM AM/PM
+    date = datetime.fromtimestamp(time_stamp).date() 
+    formatted_date = date.strftime("%b %d, %Y") # Format date as Month Day, Year
+    day_of_week = date.strftime("%A") # Get the day of the week
+
+    # Extract the temperature, wind speed, wind gusts, and weather description
     temp = interval['main']['temp']
+    wind_speed = interval['wind']['speed']
+    wind_gusts = interval['wind']['gust']
     description = interval['weather'][0]['description']
-    print(f'At {dt_txt}, the temperature will be {temp}°C with {description}.')
+
+    print(f'{formatted_time:<10} {day_of_week:<10} {formatted_date:<15} {temp:<12.1f} {description:<25} {wind_speed:<12.1f} {wind_gusts:<12.1f}')
+    print()
